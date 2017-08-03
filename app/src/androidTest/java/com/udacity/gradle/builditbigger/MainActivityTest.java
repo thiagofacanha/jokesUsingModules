802acf1062e02b85974f660ce4dfbff0573b2dfa
thiago.pereira <thiago.pereira@eldorado.org.br>
2017-08-03 08:58:21 -0300

    Finished Test class. Validating an asynctask

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -24,7 +24,7 @@ public class MainActivityTest {
             public void callJokeActivity(String result) {
                 assertTrue(!result.isEmpty());
             }
-        }.execute(instrumentationCtx).get();
+        }.execute(instrumentationCtx);
 
     }
 
